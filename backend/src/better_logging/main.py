023dedd3840500b39b3855359cfbdc4ed78643f7
b7w@isudo.ru <b7w@isudo.ru>
2020-02-28 23:48:59 +0300

    Move from sanic to aiohttp for simplicity

--- backend/src/better_logging/main.py
+++ backend/src/better_logging/main.py
@@ -1,41 +1,76 @@
 import asyncio
 import logging
+import os
+import sys
+import types
+import typing
+from logging.config import dictConfig
 
 import arrow
+from aiohttp import web
+from aiohttp.web_response import json_response
 from asyncpg import create_pool
-from sanic import Sanic
-from sanic.log import LOGGING_CONFIG_DEFAULTS
-from sanic.response import json
-from sanic_openapi import swagger_blueprint
+from asyncpg.pool import Pool
 
 from better_logging.core import date_between, parse_query, db_fetch
 
-LOGGING_CONFIG_DEFAULTS['loggers']['better_logging'] = {'level': 'INFO', 'handlers': ['console']}
+LOGGING_CONFIG_DEFAULTS = dict(
+    version=1,
+    disable_existing_loggers=False,
+    loggers={
+        "better_logging": {'level': 'INFO', 'handlers': ['console']},
+        "aiohttp.web": {'level': 'DEBUG', 'handlers': ['console']},
+        "aiohttp.access": {'level': 'DEBUG', 'handlers': ['console']},
+        "aiohttp": {'level': 'DEBUG', 'handlers': ['console']},
+    },
+    handlers={
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "generic",
+            "stream": sys.stdout,
+        },
+        "error_console": {
+            "class": "logging.StreamHandler",
+            "formatter": "generic",
+            "stream": sys.stderr,
+        },
+        "access_console": {
+            "class": "logging.StreamHandler",
+            "formatter": "access",
+            "stream": sys.stdout,
+        },
+    },
+    formatters={
+        "generic": {
+            "format": "%(asctime)s [%(process)d] [%(levelname)s] %(message)s",
+            "datefmt": "[%Y-%m-%d %H:%M:%S %z]",
+            "class": "logging.Formatter",
+        },
+        "access": {
+            "format": "%(asctime)s - (%(name)s)[%(levelname)s][%(host)s]: "
+                      + "%(request)s %(message)s %(status)d %(byte)d",
+            "datefmt": "[%Y-%m-%d %H:%M:%S %z]",
+            "class": "logging.Formatter",
+        },
+    },
+)
+
 MSK_TZ = 'Europe/Moscow'
 LOG = logging.getLogger('better_logging')
 
-app = Sanic(name='better-logging', log_config=LOGGING_CONFIG_DEFAULTS)
-app.blueprint(swagger_blueprint)
 
-app.config.setdefault('API_LICENSE_NAME', 'MIT')
-app.config.setdefault('API_VERSION', '0.1')
-app.config.setdefault('MODULES_UPDATE_TIME', '60')
-app.config.from_envvar('CONFIG_PATH')
-
-
-async def register_db(app, loop):
-    app.db = await create_pool(
-        dsn=app.config.DB_URL,
+async def register_db(app: web.Application):
+    app.config.db = await create_pool(
+        dsn=app.config.db_url,
         min_size=2,
         max_size=8,
         max_queries=48,
         max_inactive_connection_lifetime=640,
-        loop=loop
     )
     LOG.info('Create PG pool')
 
 
-async def close_connection(app, loop):
+async def close_connection(app: web.Application):
     await app.db.close()
     LOG.info('Closed PG pool')
 
@@ -52,37 +87,35 @@ async def find_modules(db):
     return [it[0] for it in rows]
 
 
-async def update_modules(app):
-    t = int(app.config.get('MODULES_UPDATE_TIME'))
+async def update_modules(app: web.Application):
+    t = app.config.modules_update_time
     while True:
-        res = await find_modules(app.db)
-        if app.config.get('modules') != res:
+        res = await find_modules(app.config.db)
+        if app.config.modules != res:
             app.config.modules = res
-            LOG.info('Update modules: %s', app.config.modules)
+            LOG.info('Update modules: %s', res)
         try:
             await asyncio.sleep(t)
         except asyncio.TimeoutError as e:
             LOG.warning(e)
 
 
-app.register_listener(register_db, 'before_server_start')
-app.register_listener(close_connection, 'after_server_stop')
-app.add_task(update_modules)
+async def update_modules_nowait(app: web.Application):
+    asyncio.create_task(update_modules(app))
 
 
-@app.route('/api/modules')
 async def modules(request):
     """
     From logging properties find all `appName`s
     """
-    res = request.app.config.get('modules', [])
+    res = request.app.get('modules', [])
     if res:
-        return json(res)
-    return json(await find_modules(request.app.db))
+        return json_response(res)
+    res = await find_modules(request.app.db)
+    return json_response(res)
 
 
-@app.route('/api/search', methods={'POST'})
-async def search(request):
+async def search(request: web.Request):
     """
     Logging Event search
     """
@@ -107,11 +140,11 @@ async def search(request):
         ORDER BY e.timestmp DESC
         LIMIT 1000
     '''
-    params = request.json
+    params = await request.json()
     time_from, time_to = date_between(params['datetime'])
     trace_id, messages = parse_query(params['query'])
     rows = await db_fetch(
-        request.app.db, sql,
+        request.app.config.db, sql,
         time_from, time_to,
         params['levels'],
         params['modules'],
@@ -131,8 +164,47 @@ async def search(request):
             logger_name=row['logger_name'],
             message=row['message']
         ))
-    return json(res)
+    return json_response(res)
+
+
+class Config:
+    db: Pool = None
+    modules: typing.List[str] = None
+    db_url: str = None
+    modules_update_time: str = 60
+
+    def __init__(self):
+        filename = os.environ.get('CONFIG_PATH', 'config.py')
+        module = types.ModuleType("config")
+        module.__file__ = filename
+        try:
+            with open(filename) as config_file:
+                exec(
+                    compile(config_file.read(), filename, "exec"),
+                    module.__dict__,
+                )
+        except IOError as e:
+            e.strerror = f"Unable to load config file ({e.strerror})"
+            raise
+
+        for key in dir(module):
+            if key.isupper():
+                setattr(self, key.lower(), getattr(module, key))
+
+
+def main():
+    dictConfig(LOGGING_CONFIG_DEFAULTS)
+    app = web.Application()
+    app.config = Config()
+
+    app.on_startup.append(register_db)
+    app.on_startup.append(update_modules_nowait)
+    app.on_cleanup.append(close_connection)
+    app.router.add_route('GET', '/api/modules', modules)
+    app.router.add_route('POST', '/api/search', search)
+
+    web.run_app(app, port=8000, print=LOG.info)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, access_log=False)
+    main()
